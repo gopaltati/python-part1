4afccd66432594b41766a1a159a79ddcb820bb3d
gopaltati <venutati9@gmail.com>
2024-07-02 13:37:41 +0530

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+print(sys.version)
+
+
+# In[17]:
+
+
+print("Hell,world!")
+
+
+# In[18]:
+
+
+print("Hello")
+print("world")
+print("how are you?")
+
+
+# In[19]:
+
+
+#python indentation
+if 5>2:
+    print("five is greater then two!")
+
+
+# In[20]:
+
+
+if 2 > 1:
+    print("two is greater then one!")
+if 6 > 2:
+    print("six is greater then two!")   
+
+
+# In[ ]:
+
+
+# python comment
+#sigle line comment
+
+
+# In[21]:
+
+
+# this is a comment
+print("hello world")
+
+
+# In[22]:
+
+
+#print("hello,world!")
+print("venu,gopal")
+
+
+# In[23]:
+
+
+#multiline comments
+
+# this is a comment
+# written in
+# more then just one line
+print("hello,world!")
+
+
+# In[24]:
+
+
+""" this is a comment
+ written in
+ more then just one line
+ """
+print("hello,world!")
+
+
+# In[38]:
+
+
+import keyword
+python_keywords = keyword.kwlist
+print(python_keywords)
+len(python_keywords)
+
+
+# In[ ]:
+
+
+
+
