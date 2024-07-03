71f0b751e04331b3d3dcfee5c1943784fe8ebdb4
gopaltati <venutati9@gmail.com>
2024-07-03 12:13:41 +0530

    Add files via upload

---
+++ python boolan.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+### python booleans values
+
+
+# In[2]:
+
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+# In[5]:
+
+
+a = 200
+b = 44
+if b > a:
+    print("b is  greater then a")
+    
+else:
+        
+    print("b is not greater then a")
+        
+
+
+# In[6]:
+
+
+a = 200
+b = 44
+if b < a:
+    print("b is  greater then a")
+    
+else:
+        
+    print("b is not greater then a")
+        
+
+
+# In[8]:
+
+
+print(bool("hello"))
+print(bool(24))
+print(bool(["apple","banana","cherry"]))
+
+
+# In[10]:
+
+
+print(bool(float))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool({}))
+print(bool([]))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
